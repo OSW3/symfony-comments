a99471234c1e643af4e8436fdd3d9834bad02ad3
Arnaud BODEL <arnaud.bodel@gmail.com>
2024-07-25 01:18:52 +0200

    -

--- src/CommentsBundle.php
+++ src/CommentsBundle.php
@@ -1,14 +1,14 @@
 <?php 
 namespace OSW3\Comments;
 
-use OSW3\Comments\Utils\ConfigurationYaml;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use OSW3\Comments\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CommentsBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        ConfigurationYaml::write($container->getParameter('kernel.project_dir'));
+        (new Configuration)->generateProjectConfig($container->getParameter('kernel.project_dir'));
     }
 }
